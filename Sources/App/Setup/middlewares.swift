38c1b38f397e5577770b322a049ff55ceec062c1
Shun <qi.shun.wang@icloud.com>
2020-04-01 17:39:46 +0800

    disable cors for testing

--- Sources/App/Setup/middlewares.swift
+++ Sources/App/Setup/middlewares.swift
@@ -3,28 +3,28 @@ import IAM
 /// Register your application's middlewares here.
 public func middlewares(config: inout MiddlewareConfig, services: inout Services) throws {
     // CORS
-    let allowedOrigin = Environment.get(AppEnvironment.CORS_ORIGINS.value, "http://localhost")
-    let corsConfig = CORSMiddleware.Configuration(
-        allowedOrigin: .custom(allowedOrigin),
-        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
-        allowedHeaders: [.accept,
-                         .authorization,
-                         .contentType,
-                         .origin,
-                         .xRequestedWith,
-                         .accessControlAllowCredentials,
-                         .accessControlAllowOrigin,
-                         .accessControlAllowHeaders,
-                         .accessControlAllowMethods,
-                         .accessControlExpose,
-                         .accessControlMaxAge,
-                         .accessControlRequestMethod,
-                         .accessControlRequestHeaders
-        ],
-        allowCredentials: true
-    )
-    
-    let corsMiddleware = CORSMiddleware(configuration: corsConfig)
+//    let allowedOrigin = Environment.get(AppEnvironment.CORS_ORIGINS.value, "http://localhost")
+//    let corsConfig = CORSMiddleware.Configuration(
+//        allowedOrigin: .custom(allowedOrigin),
+//        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+//        allowedHeaders: [.accept,
+//                         .authorization,
+//                         .contentType,
+//                         .origin,
+//                         .xRequestedWith,
+//                         .accessControlAllowCredentials,
+//                         .accessControlAllowOrigin,
+//                         .accessControlAllowHeaders,
+//                         .accessControlAllowMethods,
+//                         .accessControlExpose,
+//                         .accessControlMaxAge,
+//                         .accessControlRequestMethod,
+//                         .accessControlRequestHeaders
+//        ],
+//        allowCredentials: true
+//    )
+//
+//    let corsMiddleware = CORSMiddleware(configuration: corsConfig)
     
     
     let iamHostname = Environment.get(AppEnvironment.IAM_HOSTNAME.value, "http://localhost")
@@ -34,7 +34,7 @@ public func middlewares(config: inout MiddlewareConfig, services: inout Services
                               exceptionPaths: ["/v1/identity/check",
                                                "/v1/identity/token"])
     services.register(iamConfig)
-    config.use(corsMiddleware)
+//    config.use(corsMiddleware)
     config.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     config.use(SessionsMiddleware.self)
     //    config.use(FileMiddleware.self) // Serves files from `Public/` directory
