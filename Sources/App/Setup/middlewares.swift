270275965ab6331510675615416d6282408ede36
Shun <qi.shun.wang@icloud.com>
2020-04-01 18:03:46 +0800

    must have cors setup

--- Sources/App/Setup/middlewares.swift
+++ Sources/App/Setup/middlewares.swift
@@ -3,28 +3,28 @@ import IAM
 /// Register your application's middlewares here.
 public func middlewares(config: inout MiddlewareConfig, services: inout Services) throws {
     // CORS
-//    let allowedOrigin = Environment.get(AppEnvironment.CORS_ORIGINS.value, "http://localhost")
-//    let corsConfig = CORSMiddleware.Configuration(
-//        allowedOrigin: .custom(allowedOrigin),
-//        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
-//        allowedHeaders: [.accept,
-//                         .authorization,
-//                         .contentType,
-//                         .origin,
-//                         .xRequestedWith,
-//                         .accessControlAllowCredentials,
-//                         .accessControlAllowOrigin,
-//                         .accessControlAllowHeaders,
-//                         .accessControlAllowMethods,
-//                         .accessControlExpose,
-//                         .accessControlMaxAge,
-//                         .accessControlRequestMethod,
-//                         .accessControlRequestHeaders
-//        ],
-//        allowCredentials: true
-//    )
-//
-//    let corsMiddleware = CORSMiddleware(configuration: corsConfig)
+    let allowedOrigin = Environment.get(AppEnvironment.CORS_ORIGINS.value, "http://localhost")
+    let corsConfig = CORSMiddleware.Configuration(
+        allowedOrigin: .custom(allowedOrigin),
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept,
+                         .authorization,
+                         .contentType,
+                         .origin,
+                         .xRequestedWith,
+                         .accessControlAllowCredentials,
+                         .accessControlAllowOrigin,
+                         .accessControlAllowHeaders,
+                         .accessControlAllowMethods,
+                         .accessControlExpose,
+                         .accessControlMaxAge,
+                         .accessControlRequestMethod,
+                         .accessControlRequestHeaders
+        ],
+        allowCredentials: true
+    )
+
+    let corsMiddleware = CORSMiddleware(configuration: corsConfig)
     
     
     let iamHostname = Environment.get(AppEnvironment.IAM_HOSTNAME.value, "http://localhost")
